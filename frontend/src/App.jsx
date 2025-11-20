40dffb8d8813b173889931662bf512721e996455
Ali Ahsani <aliehsani4545@gmail.com>
2025-11-20 09:46:22 +0100

    fix: switched styling to App.css and added import

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from 'react';
+import './App.css';
 import CryptoJS from 'crypto-js';
 import CreateProcessor from './components/CreateProcessor';
 import { 
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import './App.css'  // <--- DEN HÄR RADEN ÄR AVGÖRANDE!
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
