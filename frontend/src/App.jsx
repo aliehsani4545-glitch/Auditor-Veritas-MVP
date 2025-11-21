f1a672d0f4794200a45bf1e986856bbca31bf1a7
Ali Ahsani <aliehsani4545@gmail.com>
2025-11-21 18:34:02 +0100

    sistallaq

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import './App.css';
 import CryptoJS from 'crypto-js';
 import CreateProcessor from './components/CreateProcessor';
